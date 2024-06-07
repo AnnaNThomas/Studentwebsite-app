60261107a3f08ed2294a947f729d779c62de8ce7
AnnaNThomas <annathomas2003ned@gmail.com>
2024-06-07 15:13:08 +0530

    off the loadind or refreshing

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <div>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/">Home</a>
+    <Link class="navbar-brand" to="/">Home</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/Search">Search</a>
+          <Link class="nav-link active" aria-current="page" to="/Search">Search</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/Delete">Delete</a>
+          <Link class="nav-link" to="/Delete">Delete</Link>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <Link class="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             VIEW ALL
-          </a>
+          </Link>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/ViewAll">View</a></li>
+            <li><Link class="dropdown-item" to="/ViewAll">View</Link></li>
           </ul>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" href="true">Exit</a>
+          <a class="nav-link disabled" to="true">Exit</a>
         </li>
       </ul>
       <form class="d-flex" role="search">
