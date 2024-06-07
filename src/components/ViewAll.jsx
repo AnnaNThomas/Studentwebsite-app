071cb284054a289d2246835d30ec04475bef397a
AnnaNThomas <annathomas2003ned@gmail.com>
2024-06-07 12:38:30 +0530

    React Studentapp

---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,100 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const ViewAll = () => {
+    return (
+        <div>
+            <NavBar/>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://media.istockphoto.com/id/1369754239/photo/university-student-in-white-background-stock-photo.jpg?s=612x612&w=0&k=20&c=LjFVDfjusWBjYTNliHV9DyXfApPGc8DmgBGEtfVgQ0Q=" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">ANNA N T</h5>
+                                            <p class="card-text">EXcellent Student</p>
+                                            <a href="#" class="btn btn-primary">Click Here</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://img.freepik.com/premium-photo/teenager-student-girl-yellow-pointing-finger-side_1368-40175.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Anjalee krishna p s</h5>
+                                            <p class="card-text">Good Student</p>
+                                            <a href="#" class="btn btn-primary">Click Here</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://st4.depositphotos.com/12982378/29475/i/450/depositphotos_294753034-stock-photo-happy-schoolkid-holding-digital-tablet.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Harshitha</h5>
+                                            <p class="card-text">Tiny Student</p>
+                                            <a href="#" class="btn btn-primary">Click Here</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Anna Theres v</h5>
+                                            <p class="card-text">Cheeky Student</p>
+                                            <a href="#" class="btn btn-primary">Click Here</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://img.freepik.com/premium-photo/young-pretty-woman-looking-very-shocked-surprised-universitary-student_1194-288907.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Selin jose</h5>
+                                            <p class="card-text">Best Student</p>
+                                            <a href="#" class="btn btn-primary">Click Here</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://img.freepik.com/free-photo/young-student-woman-wearing-denim-jacket-eyeglasses-holding-colorful-folders-showing-thumb-up-pink_176532-13861.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">carol p</h5>
+                                            <p class="card-text">Nice Student</p>
+                                            <a href="#" class="btn btn-primary">Click Here</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://img.freepik.com/premium-photo/young-student-ukrainian-woman-isolated-pink-background-doing-strong-gesture_1368-530079.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">selvin p</h5>
+                                            <p class="card-text">Super Student</p>
+                                            <a href="#" class="btn btn-primary">Click Here</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://img.freepik.com/premium-photo/young-woman-isolated-blue-background-with-surprise-facial-expression_231208-14290.jpg?size=626&ext=jpg&ga=GA1.1.1448711260.1707004800&semt=ais" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Neon marcus</h5>
+                                            <p class="card-text">Better Student</p>
+                                            <a href="#" class="btn btn-primary">Click Here</a>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewAll
